c06fc4d7fd1380dc67c6ef89959f4f9936ff16c3
Alex Blanchard <alex19b@gmail.com>
2016-04-04 21:06:52 -0500

    Began adding extensions

--- OSX_Responder.swift
+++ OSX_Responder.swift
@@ -11,8 +11,46 @@ import SpriteKit
 
 extension SGScene {
     
+    // handles mouse clicks and drags
     
+    override func mouseDown(theEvent: NSEvent) {
+        let location = theEvent.locationInNode(self)
+        screenInteractionStarted(location)
+    }
     
+    override func mouseDragged(theEvent: NSEvent) {
+        let location = theEvent.locationInNode(self)
+        screenInteractionMoved(location)
+    }
+    
+    override func mouseUp(theEvent: NSEvent) {
+        let location = theEvent.locationInNode(self)
+        screenInteractionEnded(location)
+    }
+    
+    override func mouseExited(theEvent: NSEvent) {
+        let location = theEvent.locationInNode(self)
+        screenInteractionEnded(location)
+    }
+    
+    // handles key presses
+    
+    override func keyDown(theEvent: NSEvent) {
+        handleKeyEvent(theEvent, keyDown: true)
+    }
+    
+    override func keyUp(theEvent: NSEvent) {
+        handleKeyEvent(theEvent, keyDown: false)
+    }
+    
+    func handleKeyEvent(event: NSEvent, keyDown: Bool) {
+        guard let characters = event.charactersIgnoringModifiers?.characters else {
+            return
+        }
+        for character in characters {
+            buttonEvent("\(character)", velocity: 1.0, pushedOn: keyDown)
+        }
+    }
     
 }
 
--- iOS_Responder.swift
+++ iOS_Responder.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import SpriteKit
+
+extension SGScene {
+    // Handle touch screen events
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            screenInteractionStarted(location)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            screenInteractionMoved(location)
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            screenInteractionEnded(location)
+        }
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        if let actualTouches = touches {
+            for touch: AnyObject in actualTouches {
+                let location = touch.locationInNode(self)
+                screenInteractionEnded(location)
+            }
+        }
+    }
+}
